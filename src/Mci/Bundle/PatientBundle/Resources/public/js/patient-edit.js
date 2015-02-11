0e0348b838e36e2e017bc52e20b947477184be5f
imran <imran3968@gmail.com>
2015-02-11 16:48:32 +0600

    imran | BDSHR-559 | validation bug fix

--- src/Mci/Bundle/PatientBundle/Resources/public/js/patient-edit.js
+++ src/Mci/Bundle/PatientBundle/Resources/public/js/patient-edit.js
@@ -95,7 +95,7 @@ function countryCode(){
     $district = $("#mci_bundle_patientBundle_patients_permanent_address_district_id").val();
     $upazila = $("#mci_bundle_patientBundle_patients_permanent_address_upazila_id").val();
 
-    if($permanentCountryCode == '050' && ($division == "" && $district == "" && $upazila == "" && $addressLine =="")){
+    if($permanentCountryCode !="" && $permanentCountryCode == '050' && ($division == "" || $district == "" || $upazila == "" || $addressLine =="")){
         return true;
     }
     if($permanentCountryCode !="" && ($permanentCountryCode != '050' && $addressLine =="")){
