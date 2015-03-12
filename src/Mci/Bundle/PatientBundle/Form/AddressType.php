8f74e6279126c7e9850c0e418e452bc1a9bff920
imran <imran3968@gmail.com>
2015-03-12 14:08:02 +0600

    imran | 000 | bug fix for permanent address update

--- src/Mci/Bundle/PatientBundle/Form/AddressType.php
+++ src/Mci/Bundle/PatientBundle/Form/AddressType.php
@@ -132,8 +132,8 @@ class AddressType extends AbstractType
             ->add('country_code', 'choice', array(
                     'attr' => array('class' => 'form-control'),
                     'choices' => $countryCode,
-                    'required'  => true,
-                    'preferred_choices' => array('050')
+                    'empty_value' => '--Please select--',
+                    'required'  => false
                 )
             );
     }
