5b4f7ea6dfb6d338bf03b467f1236d019a192fd8
Imran Rahman <imran3968@gmail.com>
2015-07-08 12:36:52 +0600

    imran | 0000 | typo fix

--- src/Mci/Bundle/PatientBundle/Resources/public/js/patient-edit.js
+++ src/Mci/Bundle/PatientBundle/Resources/public/js/patient-edit.js
@@ -90,7 +90,7 @@ function countryCode(){
     $upazila = $("#mci_bundle_patientBundle_patients_permanent_address_upazila_id").val();
 
     if($permanentCountryCode !="" && $permanentCountryCode == '050' && ($division == "" || $district == "" || $upazila == "" || $addressLine =="")){
-        return "Please Division, District and Upazila";
+        return "Please select Address line, Division, District and Upazila";
     }
 
     if($permanentCountryCode !="" && ($permanentCountryCode != '050' && $addressLine =="")){
